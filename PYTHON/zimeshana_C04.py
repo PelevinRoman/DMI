07c9aef426959877576e630d40a40612aa5f7a87
rastamaha1998@gmail.com <boom26951505@gmail.com>
2017-11-23 17:18:43 +0200

    20170928_18_10

---
+++ PYTHON/zimeshana_C04.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as plt
+
+#plt.plot([1,2,3,4])
+
+plt.plot([-2,0,1],[4,0,-3],'*',markerfacecolor='blue', markersize=50)
+
+plt.xlabel('some other numbers')
+plt.ylabel('some numbers')
+
+
+plt.show()
